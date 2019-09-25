4fe9e34f2150cfa07e4b371d7b976c8db1e00172
Matthew C <m23.cullen@gmail.com>
2019-09-25 18:17:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,27 @@
 class Owner
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.count
+    @@all.size 
+  end 
+  
+  def self.reset_all 
+  
   end 
   
   def say_species 
-    puts "I am a #{self.species}"
+    "I am a #{self.species}."
   end 
+  
 end
