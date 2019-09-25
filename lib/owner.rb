f510f97e52a03ca7197cdf5d18fb927b2eabdccf
Matthew C <m23.cullen@gmail.com>
2019-09-25 18:38:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,15 +53,19 @@ class Owner
   
   def sell_pets
     cats.each do |cat| 
-    cat.mood = "nervous"}
-    dogs.each {|dog| dog.mood = "nervous"}
-    cats.each {|cat| cat.owner = nil}
+      cat.mood = "nervous"
+      cat.owner = nil 
+    end 
+    dogs.each do |dog| 
+      dog.mood = "nervous"
+      dog.owner = nil
+    end 
     @cats.clear
-    dogs.each {|dog| dog.owner = nil}
     @dogs.clear
   end 
   
   def list_pets
+    
   end 
   
 end
